6d55579710b4760f97df23b2dd174a0fb1b56aaf
aumkarpraja <me@aumkar.ca>
2020-05-15 15:20:08 -0400

    Formatting.

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -20,26 +20,26 @@ class MovieCard extends React.Component{
         const { movie } = this.props;
         return (
             <Card className='movie-card'>
-            <CardActionArea>
-              <CardMedia
-                className='movie-image'
-                image={ `http://image.tmdb.org/t/p/w300${movie.backdrop_path}` }
-                title={movie.title}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                    { movie.title }
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                    { movie.overview }
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary" onClick={this.selectMovie}>
-                Learn More
-              </Button>
-            </CardActions>
+                <CardActionArea>
+                <CardMedia
+                    className='movie-image'
+                    image={ `http://image.tmdb.org/t/p/w300${movie.backdrop_path}` }
+                    title={ movie.title }
+                />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            { movie.title }
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            { movie.overview }
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary" onClick={ this.selectMovie }>
+                        Learn More
+                    </Button>
+                </CardActions>
           </Card>
         );
     }
--- src/components/MovieDialog.js
+++ src/components/MovieDialog.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -10,37 +9,36 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 export default class MovieDialog extends React.Component{
     render() {
         const { movie, handleClose } = this.props;
+       
         let title = null;
         let content = null;
 
         // Avoid null errors on title if movie does not exist
-        if (movie){
-            title = <DialogTitle id="form-dialog-title">{movie.title}</DialogTitle>
+        if ( movie ){
+            title = <DialogTitle id="form-dialog-title">{ movie.title }</DialogTitle>
             content =   
             <DialogContent>
-            <DialogContentText>
-                Release Date {movie.release_date}
-                <br/><br/>
-                {movie.overview}
-               
-                
-            </DialogContentText>
+                <DialogContentText>
+                    Release Date { movie.release_date }
+                    <br/><br/>
+                    { movie.overview }
+                </DialogContentText>
             </DialogContent>
         }
     return (
         <div>
-        <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
-            Open form dialog
-        </Button>
-        <Dialog open={!!movie} onClose={handleClose} aria-labelledby="form-dialog-title">
-            {title}
-            {content}
-            <DialogActions>
-            <Button onClick={handleClose} color="primary">
-                Close
+            <Button variant="outlined" color="primary" onClick={ this.handleClickOpen }>
+                Open form dialog
             </Button>
-            </DialogActions>
-        </Dialog>
+            <Dialog open={ !!movie } onClose={ handleClose } aria-labelledby="form-dialog-title">
+                { title }
+                { content }
+                <DialogActions>
+                    <Button onClick={ handleClose } color="primary">
+                        Close
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
         );
     }
